40317b2e4bfd7b12ae94a7ed6a560b5a6976173f
jetkanerva <jeti.kanerva@gmail.com>
2026-04-07 08:01:05 +0300

    before refactor

--- backend/main.py
+++ backend/main.py
@@ -67,8 +67,4 @@ app.add_middleware(
 
 # Include all routes
 app.include_router(api_router)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
    
